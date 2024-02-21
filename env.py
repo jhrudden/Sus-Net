08614cf50bb4a04dc2bfc0ac1a48d18f0741f06b
John Henry Rudden <johnhenryrudden@gmail.com>
2024-02-20 21:03:08 -0500

    add basic movement actions functionality with env

--- env.py
+++ env.py
@@ -14,13 +14,13 @@ class MoveAction(Enum):
 
 def move(action, position):
     if action == MoveAction.UP:
-        return (position[0], position[1]+1)
+        return (position[0], position[1] - 1)
     elif action == MoveAction.DOWN:
-        return (position[0], position[1]-1)
+        return (position[0], position[1] + 1)
     elif action == MoveAction.LEFT:
-        return (position[0]-1, position[1])
+        return (position[0] - 1, position[1])
     elif action == MoveAction.RIGHT:
-        return (position[0]+1, position[1])
+        return (position[0] + 1, position[1])
 
 def reverse_action(action):
     if action == MoveAction.UP:
@@ -39,25 +39,76 @@ class MazeEnv(Env):
         if random_state is not None:
             np.random.seed(random_state)
 
+        self.action_space = spaces.Discrete(4) # Need to change this to include KILL + FIX + SABOTAGE actions
+        self.observation_space = spaces.Tuple((
+            spaces.MultiDiscrete([n_rooms, n_rooms]),  # Agent positions
+            spaces.MultiDiscrete([n_rooms, n_rooms]),  # Job positions
+            spaces.MultiBinary(n_agents),  # Completed jobs
+            spaces.MultiBinary(n_agents)  # Alive agents
+        ))
+
         self.n_rooms = n_rooms
         self.n_agents = n_agents
         self._generate_rooms()
         self.reset()
 
     def reset(self):
+        """
+        Reset the environment to the initial state
+
+        Returns:
+        Tuple: A tuple containing the initial state and an empty dictionary
+        """
         self.t = 0
         rooms_list = np.arange(self.n_rooms)
         agent_pos_indices = np.random.choice(rooms_list, size=self.n_agents, replace=True)
         self.agent_positions = [self.rooms[i] for i in agent_pos_indices]
         job_pos_indices = np.random.choice(rooms_list, size=self.n_agents, replace=True)
+        self.alive_agents = np.ones(self.n_agents)
         self.job_positions = [self.rooms[i] for i in job_pos_indices]
         self.completed_jobs = np.zeros(self.n_agents)
         self.turn = 0
         # TODO: Should turn be a part of the state?
-        return ((self.agent_positions, self.job_positions, self.completed_jobs), {})
+        return ((self.agent_positions, self.job_positions, self.completed_jobs, self.alive_agents), {})
 
+    # TODO: Should we be turned based or just take a list of actions?
     def step(self, action):
-        raise NotImplementedError
+        """
+        Take a step in the environment.
+        """
+        truncated = False
+        done = False
+        if np.sum(self.alive_agents) == 0:
+            # all agents are dead
+            reward = -1
+            done = True
+        
+        elif np.sum(self.completed_jobs) == self.n_agents:
+            # all jobs are completed
+            reward = 1
+            done = True
+
+        elif self.alive_agents[self.turn] == 0:
+            # agent is dead
+            reward = -1
+        else:
+            # agent is alive
+            # TODO: Need to handle non-move actions
+            reward = 0
+            pos = self.agent_positions[self.turn]
+            if action in self.room_map[pos]:
+                self.agent_positions[self.turn] = self.room_map[pos][action]
+            else:
+                # do nothing
+                pass
+
+        # increment the turn
+        self.turn = (self.turn + 1) % self.n_agents
+
+        # TODO: Need to check if t is incremented by a wrapper or if we need to do it here
+        self.t += 1
+
+        return ((self.agent_positions, self.job_positions, self.completed_jobs, self.alive_agents), reward, done, truncated, {})
 
     def _generate_rooms(self):
         rooms = [(0,0)]
@@ -98,26 +149,39 @@ class MazeEnv(Env):
             if room in self.room_map:
                 for action, next_room in self.room_map[room].items():
                     if action == MoveAction.UP:
-                        ax.add_patch(patches.Rectangle((room_center[0] - door_width / 2, room_center[1] + room_size / 2),
-                                                    door_width, door_width, color='black'))
-                    elif action == MoveAction.DOWN:
+                        # Door on the bottom edge (visually appears at the bottom because we're not changing the x-coordinate)
+                                                    
                         ax.add_patch(patches.Rectangle((room_center[0] - door_width / 2, room_center[1] - room_size / 2 - door_width),
                                                     door_width, door_width, color='black'))
+                    elif action == MoveAction.DOWN:
+                        # Door on the top edge (visually appears at the top because we're not changing the x-coordinate)
+                        ax.add_patch(patches.Rectangle((room_center[0] - door_width / 2, room_center[1] + room_size / 2),
+                                                    door_width, door_width, color='black'))
                     elif action == MoveAction.LEFT:
+                        # Door on the right edge
                         ax.add_patch(patches.Rectangle((room_center[0] - room_size / 2 - door_width, room_center[1] - door_width / 2),
                                                     door_width, door_width, color='black'))
                     elif action == MoveAction.RIGHT:
+                        # Door on the left edge
                         ax.add_patch(patches.Rectangle((room_center[0] + room_size / 2, room_center[1] - door_width / 2),
                                                     door_width, door_width, color='black'))
+
        # Draw agents and jobs 
         for j, job in enumerate(self.job_positions):
             if self.completed_jobs[j] == 0:
-                ax.add_patch(patches.Circle(job, 0.3, color='green'))
+                ax.add_patch(patches.Circle(job, 0.3, color='yellow'))
             else:
                 ax.add_patch(patches.Circle(job, 0.3, color='red'))
 
-        for agent in self.agent_positions:
-            ax.add_patch(patches.Circle(agent, 0.3, color='black', alpha=0.3))
+        for i, agent in enumerate(self.agent_positions):
+            agent = (agent[0] - 0.5, agent[1] - 0.5)
+            if self.alive_agents[i] == 0:
+                ax.add_patch(patches.Circle(agent, 0.3, color='black', alpha=0.3))
+            else:
+                if i == self.turn:
+                    ax.add_patch(patches.Rectangle(agent, 1, 1, color='green', alpha=0.3))
+                else:
+                    ax.add_patch(patches.Rectangle(agent, 1, 1, color='gray', alpha=0.3))
                     
         ax.set_aspect('equal')
         plt.gca().invert_yaxis()  # Invert y-axis to match the coordinate system used in your environment
