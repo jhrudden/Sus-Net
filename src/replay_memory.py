c40727188eb455937a48ad77502e6adb383ed3d2
jhrudden <johnhenryrudden@gmail.com>
2024-04-16 16:08:32 -0400

    fast buffer

--- src/replay_memory.py
+++ src/replay_memory.py
@@ -1,5 +1,6 @@
 import torch
 from collections import namedtuple
+from src.utils import EnhancedOrderedDict
 
 # Batch namedtuple, i.e. a class which contains the given attributes
 Batch = namedtuple("Batch", ("states", "actions", "rewards", "next_states", "dones"))
@@ -102,6 +103,120 @@ class TrajectoryReplayBuffer:
             rewards=self.rewards[idx],
             next_states=self.next_states[idx],
             dones=self.dones[idx],
+        )
+
+    def populate(self, env, num_steps):
+        """Populate this replay memory with `num_steps` from the random policy.
+
+        :param env: Gymnasium environment
+        :param num_steps: Number of steps to populate the replay memory
+        """
+
+        # TODO
+
+        pass
+
+
+class FastReplayBuffer:
+    def __init__(self, max_size, trajectory_size, state_size):
+
+        self.max_size = max_size
+        self.trajectory_size = trajectory_size
+        self.state_size = state_size
+
+        self.trajectory_dict = EnhancedOrderedDict(max_size)
+
+        # initalizing the timestep buffer
+        self.states = torch.empty((max_size, state_size))
+        self.actions = torch.empty((max_size, 1), dtype=torch.long)
+        self.rewards = torch.empty((max_size, 1))
+        self.next_states = torch.empty((max_size, state_size))
+        self.dones = torch.empty((max_size, 1), dtype=torch.bool)
+        self.starts = torch.empty((max_size, 1), dtype=torch.bool)
+
+        # initializing current index and buffer size
+        self.idx = 0
+        self.size = 0
+
+        # initializing the trajectory length tracker
+        self.trajectory_lengths = torch.zeros(max_size)
+
+    def add(self, state, action, reward, next_state, done, is_start: bool = False):
+        """Add a transition to the buffer.
+
+        :param state: 1-D np.ndarray of state-features
+        :param action: Integer action
+        :param reward: Float reward
+        :param next_state: 1-D np.ndarray of state-features
+        :param done: Boolean value indicating the end of an episode
+        """
+        # check if we are overwriting a trajectory
+        # if we are, then pop trajectory_size - 1 elemnts from the smart_buffer or until we reach the start of an episode
+        if self.trajectory_dict.has(self.idx):
+            self.starts[self.idx] = False
+            for i in range(self.trajectory_size):
+                new_idx = (self.idx + i) % self.max_size
+                if self.starts[new_idx]:
+                    break
+                self.trajectory_dict.pop()
+
+        self.states[self.idx] = torch.tensor(state)
+        self.actions[self.idx] = torch.tensor(action)
+        self.rewards[self.idx] = torch.tensor(reward)
+        self.next_states[self.idx] = torch.tensor(next_state)
+        self.dones[self.idx] = torch.tensor(done)
+        self.starts[self.idx] = torch.tensor(is_start)
+        
+        self.trajectory_dict.insert(self.idx)
+
+        # Circulate the pointer to the next position
+        self.idx = (self.idx + 1) % self.max_size
+        # Update the current buffer size
+        self.size = min(self.size + 1, self.max_size)
+
+        # # self-padding (initial time step of episode is padded to fit trajectory sequence)
+        # if timestep == 0:
+        #     for i in range(1, self.trajectory_size):
+        #         self.add(state, action, reward, next_state, done, i)
+
+    def sample(self, batch_size) -> Batch:
+        """Sample a batch of experiences.
+
+        If the buffer contains less that `batch_size` transitions, sample all
+        of them.
+
+        :param batch_size: Number of transitions to sample
+        :rtype: Batch
+        """
+
+        sample_idx = torch.tensor(self.trajectory_dict.sample(n_samples=batch_size), dtype=torch.int)
+        print(sample_idx)
+
+        seq = torch.ones((batch_size, self.trajectory_size), dtype=torch.int) * -1
+
+        for i in range(self.trajectory_size):
+            new_idx = (sample_idx - i) % self.max_size
+            neg = seq[:, i] == -1
+
+            seq[neg, i] = new_idx[neg].squeeze()
+
+            starts = self.starts[new_idx][neg].squeeze()
+            if torch.any(starts) and i < self.trajectory_size - 1:
+                seq[starts, i:] = new_idx[starts].unsqueeze(1).repeat(1, self.trajectory_size - i)
+
+            if not torch.any(neg):
+                break
+            
+        
+        seq = torch.flip(seq, dims=[1])
+        print(seq)
+
+        return Batch(
+            states=self.states[seq],
+            actions=self.actions[seq],
+            rewards=self.rewards[seq],
+            next_states=self.next_states[seq],
+            dones=self.dones[seq],
         )
 
     def populate(self, env, num_steps):
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from typing import Union, Any
+import torch
+
 def calculate_cnn_output_dim(input_size, kernel_sizes, strides, paddings):
     output_size = input_size
 
@@ -5,3 +8,50 @@ def calculate_cnn_output_dim(input_size, kernel_sizes, strides, paddings):
         output_size = ((output_size - kernel_size + 2 * padding) // stride) + 1
 
     return output_size
+
+class EnhancedOrderedDict():
+    def __init__(self, max_size: int):
+        self.set = set()
+        self.keys = []
+        self.size = 0 # used for lazy deletion of keys
+        self.tail = 0
+        self.max_size = max_size
+
+    # use head and tail for array to handle lazy deletion for O(1) time complexity
+    def insert(self, key: Union[str, int]):
+        if self.size + 1 > self.max_size:
+            # delete the tail element
+            self.set.remove(self.keys[self.tail])
+            self.tail = (self.tail + 1) % self.max_size
+            self.size -= 1
+        
+        # NOTE: going to assume that the key is unique
+        self.set.add(key)
+        # append at tail if size is less than max_size
+        if self.tail > 0:
+            self.keys[self.tail-1] = key
+            self.size += 1
+        else:
+            self.keys.append(key)
+            self.size += 1
+    
+    def pop(self):
+        """
+        Remove from tail
+        """
+        if self.size == 0:
+            return None
+        
+        key = self.keys[self.tail]
+        self.set.remove(key)
+        self.tail = (self.tail + 1) % self.max_size
+        self.size -= 1
+        return key
+    
+    def has(self, key: Union[str, int]) -> bool:
+        return key in self.set
+        
+    def sample(self, n_samples: int = 1):
+        cut = torch.randint(0, self.size, (n_samples,))
+        idx_in_keys = (cut + self.tail) % self.max_size
+        return [self.keys[i] for i in idx_in_keys]
