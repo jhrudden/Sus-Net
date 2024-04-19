c8b86937fac238a6f9c1e028052d55f31914f0da
Dima Vremekno <dimavrem22@gmail.com>
2024-04-19 01:07:17 -0400

    train progress

--- src/train.py
+++ src/train.py
@@ -1,7 +1,11 @@
 from typing import List
 from scheduler import ExponentialSchedule
 from src.env import FourRoomEnv, AgentTypes, StateFields
-from src.featurizers import PerspectiveFeaturizer, StateSequenceFeaturizer
+from src.featurizers import (
+    PerspectiveFeaturizer,
+    StateSequenceFeaturizer,
+    GlobalFeaturizer,
+)
 from src.replay_memory import FastReplayBuffer
 from src.models.dqn import SpatialDQN, RandomEquiprobable
 import numpy as np
@@ -21,30 +25,78 @@ def run_experiment(
     replay_prepopulate_steps: int = 1000,
     batch_size: int = 32,
     gamma: float = 0.99,
-    scheduler: ExponentialSchedule = ExponentialSchedule(1.0, 0.05, 1_000_000),
+    scheduler_start_eps: float = 1.0,
+    scheduler_end_eps: float = 0.05,
+    scheduler_time_steps: int = 1_000_000,
     who_to_train: AgentTypes = AgentTypes.AGENT,
     imposter_pretrained_model_path: str | None = None,
     crew_pretrained_model_path: str | None = None,
     experiment_save_path: str | None = None,
     optimizer_type: str = "Adam",
     learning_rate: float = 0.0001,
+    train_step_interval: int = 5,
+    num_checkpoint_saves: int = 5,
 ):
     # initializing models
     if imposter_model_type == "spatial_dqn":
-        # TODO: MODEL INIT
-        imposter_model = None
+
+        imposter_model = SpatialDQN(
+            input_image_size=env.n_cols,
+            non_spatial_input_size=5,
+            n_channels=[9, 9, 9],
+            strides=[1, 1, 2],
+            paddings=[
+                2,
+                2,
+                2,
+            ],
+            kernel_sizes=[2, 3, 2],
+            rnn_model="rnn",
+            rnn_layers=3,
+            rnn_hidden_dim=224,
+            rnn_dropout=0.2,
+            mlp_hidden_layer_dims=[16, 16],
+            n_actions=env.n_imposter_actions,
+        )
     elif imposter_model_type == "random":
         imposter_model = RandomEquiprobable(env.n_imposter_actions)
     else:
         raise ValueError(f"Invalid model type: {imposter_model_type}")
 
-    if imposter_model_type == "spatial_dqn":
-        # TODO: MODEL INIT
-        crew_model = None
-    elif imposter_model_type == "random":
+    # loading model checkpoint if provided
+    if imposter_pretrained_model_path is not None:
+        # TODO: load model
+        pass
+
+    if crew_model_type == "spatial_dqn":
+
+        crew_model = SpatialDQN(
+            input_image_size=env.n_cols,
+            non_spatial_input_size=5,
+            n_channels=[9, 9, 9],
+            strides=[1, 1, 2],
+            paddings=[
+                2,
+                2,
+                2,
+            ],
+            kernel_sizes=[2, 3, 2],
+            rnn_model="rnn",
+            rnn_layers=3,
+            rnn_hidden_dim=224,
+            rnn_dropout=0.2,
+            mlp_hidden_layer_dims=[16, 16],
+            n_actions=env.n_imposter_actions,
+        )
+    elif crew_model_type == "random":
         crew_model = RandomEquiprobable(env.n_crew_actions)
     else:
         raise ValueError(f"Invalid model type: {imposter_model_type}")
+
+    # loading model checkpoint if provided
+    if crew_pretrained_model_path is not None:
+        # TODO: load model
+        pass
 
     # determining which parameters to optimize: who are we training
     if who_to_train == AgentTypes.AGENT:
@@ -60,22 +112,63 @@ def run_experiment(
     else:
         raise ValueError(f"Invalid optimizer: {optimizer_type}")
 
+    # initialize scheduler
+    scheduler = ExponentialSchedule(
+        scheduler_start_eps, scheduler_end_eps, scheduler_time_steps
+    )
+
+    # initialize replay buffer and prepopulate it
+    replay_buffer = FastReplayBuffer(
+        max_size=replay_buffer_size,
+        trajectory_size=sequence_length,
+        state_size=env.flattened_state_size,
+        n_imposters=env.n_imposters,
+        n_agents=env.n_agents,
+    )
+    replay_buffer.populate(env=env, num_steps=replay_prepopulate_steps)
+
+    # initialize featurizer
+    if featurizer_type == "perspective":
+        featurizer = PerspectiveFeaturizer(env=env, sequence_len=sequence_length)
+    elif featurizer_type == "global":
+        featurizer = GlobalFeaturizer(env=env, sequence_len=sequence_length)
+
+    # run actual experiment
+    train(
+        env=env,
+        num_steps=num_steps,
+        featurizer=featurizer,
+        optimizer=optimizer,
+        imposter_model=imposter_model,
+        crew_model=crew_model,
+        save_directory_path=experiment_save_path,
+        train_step_interval=train_step_interval,
+        batch_size=batch_size,
+        gamma=gamma,
+        scheduler=scheduler,
+        who_to_train=who_to_train,
+        num_saves=num_checkpoint_saves,
+    )
+
+    # run experiment
+    if experiment_save_path is not None:
+        # TODO: SAVE some stuff!!!!
+        pass
+
 
 def train(
     env: FourRoomEnv,
     num_steps: int,
-    n_imposter_actions: int,
-    n_crew_actions: int,
     replay_buffer: FastReplayBuffer,
     featurizer: StateSequenceFeaturizer,
     optimizer: torch.optim.optimizer,
     imposter_model: nn.Module,
     crew_model: nn.Module,
+    scheduler: ExponentialSchedule,
     save_directory_path: str,
     train_step_interval: int = 5,
     batch_size: int = 32,
     gamma: float = 0.99,
-    scheduler: ExponentialSchedule = ExponentialSchedule(1.0, 0.05, 1_000_000),
     who_to_train: AgentTypes = AgentTypes.AGENT,  # trains both imposters and crew by default
     num_saves: int = 5,
 ):
@@ -127,7 +220,9 @@ def train(
             # choose action for alive imposter
             if env.imposter_mask[agent_idx] and alive_agents[agent_idx]:
                 if np.random.random() <= eps:
-                    agent_actions[agent_idx] = np.random.randint(0, n_imposter_actions)
+                    agent_actions[agent_idx] = np.random.randint(
+                        0, env.n_imposter_actions
+                    )
                 else:
                     agent_actions[agent_idx] = int(
                         torch.argmax(imposter_model(spatial, non_spatial))
@@ -136,7 +231,7 @@ def train(
             # choose action for alive crew member
             elif alive_agents[agent_idx]:
                 if np.random.random() <= eps:
-                    agent_actions[agent_idx] = np.random.randint(0, n_crew_actions)
+                    agent_actions[agent_idx] = np.random.randint(0, env.n_crew_actions)
                 else:
                     agent_actions[agent_idx] = int(
                         torch.argmax(crew_model(spatial, non_spatial))
