9e56d69d08729c2633268c9aa527b8af6472e609
Dima Vremekno <dimavrem22@gmail.com>
2024-04-22 21:39:55 -0400

    imposter env end game

--- src/env.py
+++ src/env.py
@@ -561,7 +561,8 @@ Metrics:
 
 class ImposterTrainingGround(FourRoomEnv):
     """
-    Environment used to train an imposter to act against random, equiprobable crew members.
+    A specialized environment where an imposter is trained to strategize against
+    a set of crew members that perform random actions with equal probability.
     """
 
     def __init__(
@@ -575,6 +576,19 @@ class ImposterTrainingGround(FourRoomEnv):
         random_state=None,
         debug=False,
     ):
+        """
+        Initializes the ImposterTrainingGround environment.
+
+        Parameters:
+            n_crew (int): Number of crew members.
+            n_jobs (int): Number of jobs available for crew members to complete.
+            time_step_reward (float): Reward received at each timestep.
+            kill_reward (float): Reward for killing a crew member.
+            sabotage_reward (float): Reward for sabotaging a job.
+            end_of_game_reward (float): Reward or penalty at the end of the game.
+            random_state (int, optional): Seed for the random number generator.
+            debug (bool): Flag to enable debugging outputs.
+        """
         super().__init__(
             n_imposters=1,
             n_crew=n_crew,
@@ -590,6 +604,15 @@ class ImposterTrainingGround(FourRoomEnv):
         )
 
     def step(self, imposter_action):
+        """
+        Processes the imposter's action and generates actions for the crew members.
+
+        Parameters:
+            imposter_action (int or list): The action chosen by the imposter.
+
+        Returns:
+            tuple: Contains the new state, imposter's reward, and game termination status.
+        """
 
         # convert a list to an integer if a list of action is provided
         if isinstance(imposter_action, list):
@@ -608,6 +631,25 @@ class ImposterTrainingGround(FourRoomEnv):
         reward = rewards[self.imposter_idxs[0]]
 
         return next_state, reward, done, trunc, metrics
+
+    def check_win_condition(self):
+        """
+        Checks if the game has reached a win condition for the imposter or the crew.
+
+        Returns:
+            tuple: A boolean indicating if the game has ended and the associated reward.
+        """
+
+        # all jobs are done imposter looses
+        # NOTE: this is only possible if n_jobs is not 0
+        if self.n_jobs != 0 and self.completed_jobs.sum() == self.n_jobs:
+            return True, self.game_end_reward
+
+        # imposter wins bu killing all crew
+        if self.alive_agents[self.crew_mask].sum() == 0:
+            return True, -1 * self.game_end_reward
+
+        return False, 0
 
 
 class FourRoomEnvWithTagging(FourRoomEnv):
